16ab06ce9337e819f27998e361fde99b6d5b36ee
unknown <hevenlych@gmail.com>
2019-08-21 19:55:58 +0900

    something goes wrong

--- src/Routes/Search/SearchPresenter.js
+++ src/Routes/Search/SearchPresenter.js
@@ -3,6 +3,8 @@ import PropTypes from "prop-types";
 import styled from "styled-components";
 import Loader from "Components/Loader";
 import Section from "Components/Section";
+import Message from "../../Components/Message";
+import Poster from "Components/Poster";
 
 const Container = styled.div`
   padding: 0 20px;
@@ -43,17 +45,42 @@ const SearchPresenter = ({
         {movieResults && movieResults.length > 0 && (
           <Section title="Movie Results">
             {movieResults.map(movie => (
-              <span key={movie.id}>{movie.title}</span>
+              <Poster
+                key={movie.id}
+                id={movie.id}
+                title={movie.original_title}
+                imageUrl={movie.poster_path}
+                rating={movie.vote_average}
+                isMovie={true}
+                year={movie.release_date.substring(0, 4)}
+              />
             ))}
           </Section>
         )}
         {tvResults && tvResults.length > 0 && (
           <Section title="TV Results">
             {tvResults.map(show => (
-              <span key={show.id}>{show.name}</span>
+              <Poster
+                key={show.id}
+                id={show.id}
+                title={show.original_name}
+                imageUrl={show.poster_path}
+                rating={show.vote_average}
+                year={show.first_air_date.substring(0, 4)}
+              />
             ))}
           </Section>
         )}
+        {error && <Message color="#e74c3c" text={error} />}
+        {tvResults &&
+          movieResults &&
+          tvResults.length === 0 &&
+          movieResults.length === 0 && (
+            <Message
+              text={`Nothing found for: ${searchTerm}`}
+              color="#95a5a6"
+            />
+          )}
       </>
     )}
   </Container>
